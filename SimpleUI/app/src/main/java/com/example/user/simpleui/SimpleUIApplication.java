48bd56a7fb02ed77d9b4a1a2919ab758d53cf8d6
hippoyiyi <hippoyiyi@gmail.com>
2016-07-19 12:53:37 +0800

    SimpleUIApplication

--- SimpleUI/app/src/main/java/com/example/user/simpleui/SimpleUIApplication.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/SimpleUIApplication.java
@@ -13,11 +13,12 @@ public class SimpleUIApplication extends Application {
     public void onCreate(){
         super.onCreate();
         ParseObject.registerSubclass(Order.class);
+        ParseObject.registerSubclass(Drink.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("3ae4vlj89AYKXvskcaWvXC9DEq7RaIIcuxciqIgR")
                 .server("https://parseapi.back4app.com/")
                 .clientKey("ePzhoiz1URqSWA4hYcqx6DdJWSA2Zns1ChApdnSj")
-                        .enableLocalDataStore()
+                .enableLocalDataStore()
                         //.applicationId("76ee57f8e5f8bd628cc9586e93d428d5")
                         //.server("http://parseserver-ps662-env.us-east-1.elasticbeanstalk.com/parse/")
                 .build());
