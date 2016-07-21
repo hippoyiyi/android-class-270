e195caf72be65debece60cd5cf75882592a1c80e
hippoyiyi <hippoyiyi@gmail.com>
2016-07-21 10:09:56 +0800

    get static map from latlng

--- SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
@@ -5,12 +5,14 @@ import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import java.lang.ref.WeakReference;
 import java.util.List;
 import java.util.logging.Handler;
 
@@ -29,6 +31,7 @@ public class OrderDetailActivity extends AppCompatActivity {
         TextView noteTextView = (TextView)findViewById(R.id.noteTextView);
         TextView menuResultsTextView = (TextView)findViewById(R.id.menuResultsTextView);
         TextView storeInfoTextView = (TextView)findViewById(R.id.storeInfoTextView);
+        ImageView staticMapImageView = (ImageView)findViewById(R.id.googleMapImageView);
         noteTextView.setText(note);
         storeInfoTextView.setText(storeInfo);
 
@@ -50,19 +53,33 @@ public class OrderDetailActivity extends AppCompatActivity {
                 Handler
             }
         }).run();*/
-
+          new GeoCodingTask(staticMapImageView).execute("台北市羅斯福路四段一號");
     }
+
     public static class GeoCodingTask extends AsyncTask<String, Void, Bitmap>{
+
+        WeakReference<ImageView> imageViewWeakReference;
+
         @Override
         protected Bitmap doInBackground(String... params) {
             String address = params[0];
                 double[] latlng = Utils.getLatLngFromGoogleMapAPI(address);
-            return null;
+            return Utils.getStatisticMap(latlng);
         }
 
         @Override
         protected void onPostExecute(Bitmap bitmap) {
+
             super.onPostExecute(bitmap);
+            if(imageViewWeakReference.get() != null && bitmap != null)
+            {
+                ImageView imageView = imageViewWeakReference.get();
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+        public GeoCodingTask(ImageView imageView)
+        {
+           this.imageViewWeakReference = new WeakReference<ImageView>(imageView);
         }
     }
 }
--- SimpleUI/app/src/main/java/com/example/user/simpleui/Utils.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/Utils.java
@@ -1,6 +1,8 @@
 package com.example.user.simpleui;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -40,7 +42,7 @@ public class Utils {
         try {
             FileInputStream fileInputStream = context.openFileInput(fileName);
             byte[] buffer = new byte[2048];
-            fileInputStream.read(buffer, 0 ,buffer.length);
+            fileInputStream.read(buffer, 0, buffer.length);
             fileInputStream.close();
             return new String(buffer);
         } catch (FileNotFoundException e) {
@@ -102,6 +104,20 @@ public class Utils {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        return null;
+    }
+    public static Bitmap getStatisticMap(double[] latlng)
+    {
+        String center = String.valueOf(latlng[0])+","+String.valueOf(latlng[1]);
+        String staticMapURL = "http://map.google.com/maps/api/staticmap?center="+center+"&size=640x480&zoom=17";
+
+        byte[] bytes = Utils.urlToBytes(staticMapURL);
+        if(bytes != null)
+        {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            return bitmap;
+        }
+
         return null;
     }
 }
