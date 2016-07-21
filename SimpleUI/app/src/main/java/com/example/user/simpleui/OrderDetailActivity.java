6fca7e66f72b22f321f79217020f4bf29092752e
hippoyiyi <hippoyiyi@gmail.com>
2016-07-21 12:36:58 +0800

    finish

--- SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
@@ -2,21 +2,39 @@ package com.example.user.simpleui;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+//import com.google.android.gms.appindexing.Action;
+//import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.lang.ref.WeakReference;
 import java.util.List;
 import java.util.logging.Handler;
 
-public class OrderDetailActivity extends AppCompatActivity {
+public class OrderDetailActivity extends AppCompatActivity implements GeoCodingTask.GeoCodingResponse {
+
+    GoogleMap googleMap;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,24 +46,33 @@ public class OrderDetailActivity extends AppCompatActivity {
         String menuResults = intent.getStringExtra("menuResults");
         String storeInfo = intent.getStringExtra("storeInfo");
 
-        TextView noteTextView = (TextView)findViewById(R.id.noteTextView);
-        TextView menuResultsTextView = (TextView)findViewById(R.id.menuResultsTextView);
-        TextView storeInfoTextView = (TextView)findViewById(R.id.storeInfoTextView);
-        ImageView staticMapImageView = (ImageView)findViewById(R.id.googleMapImageView);
+        TextView noteTextView = (TextView) findViewById(R.id.noteTextView);
+        TextView menuResultsTextView = (TextView) findViewById(R.id.menuResultsTextView);
+        TextView storeInfoTextView = (TextView) findViewById(R.id.storeInfoTextView);
+        ImageView staticMapImageView = (ImageView) findViewById(R.id.googleMapImageView);
         noteTextView.setText(note);
         storeInfoTextView.setText(storeInfo);
 
         List<String> menuResultList = Order.getMenuResultList(menuResults);
 
         String text = "";
-        if(menuResultList != null)
-        {
-            for(String menuResult : menuResultList)
-            {
+        if (menuResultList != null) {
+            for (String menuResult : menuResultList) {
                 text += menuResult + "\n";
             }
         }
-             menuResultsTextView.setText(text);
+        menuResultsTextView.setText(text);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap map) {
+                googleMap = map;
+                (new GeoCodingTask(OrderDetailActivity.this)).execute("台北市羅斯福路四段一號");
+            }
+        });
+
 
               /* new Thread(new Runnable(){
             @Override
@@ -53,33 +80,94 @@ public class OrderDetailActivity extends AppCompatActivity {
                 Handler
             }
         }).run();*/
-          new GeoCodingTask(staticMapImageView).execute("台北市羅斯福路四段一號");
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public static class GeoCodingTask extends AsyncTask<String, Void, Bitmap>{
+    //    public static class GeoCodingTask extends AsyncTask<String, Void, Bitmap>{
+//
+//        WeakReference<ImageView> imageViewWeakReference;
+//
+//        @Override
+//        protected Bitmap doInBackground(String... params) {
+//            String address = params[0];
+//                double[] latlng = Utils.getLatLngFromGoogleMapAPI(address);
+//            return Utils.getStatisticMap(latlng);
+//        }
+//
+//        @Override
+//        protected void onPostExecute(Bitmap bitmap) {
+//
+//            super.onPostExecute(bitmap);
+//            if(imageViewWeakReference.get() != null && bitmap != null)
+//            {
+//                ImageView imageView = imageViewWeakReference.get();
+//                imageView.setImageBitmap(bitmap);
+//            }
+//        }
+//        public GeoCodingTask(ImageView imageView)
+//        {
+//           this.imageViewWeakReference = new WeakReference<ImageView>(imageView);
+//        }
+//    }
+    public void responseWithGeoCodingResults(LatLng latlng) {
+        if (googleMap != null) {
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latlng, 17);
+            //googleMap.animateCamera(cameraUpdate);
+            MarkerOptions markerOptions = new MarkerOptions().position(latlng).title("台灣大學").snippet("Hello Google Map");
+            googleMap.addMarker(markerOptions);
 
-        WeakReference<ImageView> imageViewWeakReference;
-
-        @Override
-        protected Bitmap doInBackground(String... params) {
-            String address = params[0];
-                double[] latlng = Utils.getLatLngFromGoogleMapAPI(address);
-            return Utils.getStatisticMap(latlng);
+            googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                @Override
+                public boolean onMarkerClick(Marker marker) {
+                    CameraUpdate cp = CameraUpdateFactory.newLatLngZoom(marker.getPosition(),21);
+                    googleMap.moveCamera(cp);
+                    return false;
+                }
+            });
+            googleMap.moveCamera(cameraUpdate);
         }
+    }
 
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
+    @Override
+    public void onStart() {
+        super.onStart();
 
-            super.onPostExecute(bitmap);
-            if(imageViewWeakReference.get() != null && bitmap != null)
-            {
-                ImageView imageView = imageViewWeakReference.get();
-                imageView.setImageBitmap(bitmap);
-            }
-        }
-        public GeoCodingTask(ImageView imageView)
-        {
-           this.imageViewWeakReference = new WeakReference<ImageView>(imageView);
-        }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "OrderDetail Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.user.simpleui/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "OrderDetail Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.user.simpleui/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
--- SimpleUI/app/src/main/java/com/example/user/simpleui/SimpleUIApplication.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/SimpleUIApplication.java
@@ -15,12 +15,12 @@ public class SimpleUIApplication extends Application {
         ParseObject.registerSubclass(Order.class);
         ParseObject.registerSubclass(Drink.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
-                //.applicationId("3ae4vlj89AYKXvskcaWvXC9DEq7RaIIcuxciqIgR")
-                //.server("https://parseapi.back4app.com/")
-                //.clientKey("ePzhoiz1URqSWA4hYcqx6DdJWSA2Zns1ChApdnSj")
+                .applicationId("3ae4vlj89AYKXvskcaWvXC9DEq7RaIIcuxciqIgR")
+                .server("https://parseapi.back4app.com/")
+                .clientKey("ePzhoiz1URqSWA4hYcqx6DdJWSA2Zns1ChApdnSj")
                 .enableLocalDataStore()
-                        .applicationId("76ee57f8e5f8bd628cc9586e93d428d5")
-                        .server("http://parseserver-ps662-env.us-east-1.elasticbeanstalk.com/parse/")
+                        //.applicationId("76ee57f8e5f8bd628cc9586e93d428d5")
+                        //.server("http://parseserver-ps662-env.us-east-1.elasticbeanstalk.com/parse/")
                 .build());
 
 
